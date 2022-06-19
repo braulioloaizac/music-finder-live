46640b5f1e4858f0dac36754269be16fe81ef4c6
braulioloaizac <bloaizac@uqvirtual.edu.co>
2022-06-18 21:15:01 -0400

    working request and showing results on screen

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 
+//Lyrics empty string
 var songWords = "";
 
 $("form").on("click","#btn", function(event){
@@ -24,29 +25,43 @@ $("form").on("click","#btn", function(event){
 
 
 var getSong = function(){
-
-//var requestUrlSong = "https://api.musixmatch.com/ws/1.1/track.search?apikey=9d4d24aaeb01794337b053fca031b0f9&q_lyrics="+songWords+"&page_size=3&page=1&s_track_rating=desc";
-
-var requestUrlSong = "https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/track.search?q_artist=kanye&q_track=ultralight&apikey=9d4d24aaeb01794337b053fca031b0f9"
-    //Makes a request to the weather API
-    
-    
-    fetch(requestUrlSong).then(function(response){
-        if (response.ok){
-            response.json().then(function(data){
-               
-                    
-                    //console.log(data.message.body.track_list)
-                    console.log(response);
-                
-            }  
-         )
+  
+    //Ajax request to get the list of songs that have the lyrics on it 
+    $.ajax({
+        type: "GET",
+        data: {
+            apikey:"9d4d24aaeb01794337b053fca031b0f9",
+            q_lyrics: songWords,
+            s_track_rating: "desc",
+            page_size: 15,
+            format:"jsonp",
+            callback:"jsonp_callback"
+        },
+        url: "https://api.musixmatch.com/ws/1.1/track.search",
         
+        dataType: "jsonp",
+        jsonpCallback: 'jsonp_callback',
+        contentType: 'application/json',
+        success: function(data) {
+            //console.log(data);
+            setInfo(data);
         }
-        else{
-            alert("Bad request");
-        }
-       
     })
+}
 
+var setInfo = function(data){
+
+    //Creates the result list
+    for(var i = 0; i < 15; i++){
+
+        //Extract the song name of each possible result
+        var songTitle = data.message.body.track_list[i].track.track_name;
+        var artist = data.message.body.track_list[i].track.artist_name;
+        console.log(songTitle)
+        console.log(artist)
+        //Creates the element on the DOM
+        var pSong= $('<li class="pSong">'+ songTitle +' by '+ artist +'</li>');
+        $("#songList").append(pSong); 
+    }
+    
 }
