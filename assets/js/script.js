bf2eb1fa62dfbd8b777bfbfcaa08229d4e3408fc
Derrick James <ballyouth@gmail.com>
2022-06-20 12:47:37 -0400

    no change yet

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,28 +25,17 @@ $("form").on("click","#btn", function(event){
 
 var getSong = function(){
 
-//var requestUrlSong = "https://api.musixmatch.com/ws/1.1/track.search?apikey=9d4d24aaeb01794337b053fca031b0f9&q_lyrics="+songWords+"&page_size=3&page=1&s_track_rating=desc";
+// var requestUrlSong = "http://api.musixmatch.com/ws/1.1/track.search?q_lyrics=" + songWords +"&";
 
-var requestUrlSong = "https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/track.search?q_artist=kanye&q_track=ultralight&apikey=9d4d24aaeb01794337b053fca031b0f9"
-    //Makes a request to the weather API
-    
-    
+// var requestUrlSong = "https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/track.search?q_lyrics="+ songWords +"&q_track=ultralight&apikey=9d4d24aaeb01794337b053fca031b0f9";
+// var requestUrlSong = "http://api.musixmatch.com/ws/1.1/track.search?q_lyrics="+ songWords +"&q_track=ultralight&apikey=9d4d24aaeb01794337b053fca031b0f9";
+
+   
     fetch(requestUrlSong).then(function(response){
         if (response.ok){
-            response.json().then(function(data){
-               
-                    
-                    //console.log(data.message.body.track_list)
-                    console.log(response);
-                
-            }  
-         )
-        
-        }
-        else{
-            alert("Bad request");
-        }
-       
-    })
+            response.json().
+            then(function(data){
+                    console.log(data.body);    
+            })}});
 
 }
