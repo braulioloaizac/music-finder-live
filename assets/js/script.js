d89801aca69d5372f288de7229a0b2fbce93036f
braulioloaizac <bloaizac@uqvirtual.edu.co>
2022-06-19 20:12:16 -0400

    obtaining data from musixmatch and passing it to youtube api and getting the video by videoId

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,10 @@
 
 //Lyrics empty string
 var songWords = "";
+//Results number
+var nResults = 8;
+
+var songTitle = "";
 
 $("form").on("click","#btn", function(event){
     event.preventDefault();
@@ -33,7 +37,7 @@ var getSong = function(){
             apikey:"9d4d24aaeb01794337b053fca031b0f9",
             q_lyrics: songWords,
             s_track_rating: "desc",
-            page_size: 15,
+            page_size: nResults,
             format:"jsonp",
             callback:"jsonp_callback"
         },
@@ -43,25 +47,66 @@ var getSong = function(){
         jsonpCallback: 'jsonp_callback',
         contentType: 'application/json',
         success: function(data) {
-            //console.log(data);
+            console.log(data);
             setInfo(data);
         }
     })
 }
 
 var setInfo = function(data){
+    
+    //Removes the previous results
+    $( ".pSong" ).remove();
 
     //Creates the result list
-    for(var i = 0; i < 15; i++){
+    for(var i = 0; i < nResults; i++){
 
         //Extract the song name of each possible result
-        var songTitle = data.message.body.track_list[i].track.track_name;
-        var artist = data.message.body.track_list[i].track.artist_name;
-        console.log(songTitle)
-        console.log(artist)
+        songTitle = data.message.body.track_list[i].track.track_name;
+        artist = data.message.body.track_list[i].track.artist_name;
+        
         //Creates the element on the DOM
         var pSong= $('<li class="pSong">'+ songTitle +' by '+ artist +'</li>');
         $("#songList").append(pSong); 
     }
     
+}
+
+
+$("#songList").on("click","li", function(event){
+    event.preventDefault();
+
+    songTitle = $(this).text();
+    videoFinder();
+})
+
+var videoFinder = function(){
+    //Youtube API KEY AIzaSyAm8wLpmDSB6Bv6QoTuaAHBiw9wpjBIZyc
+
+    $.ajax({
+        type: 'GET',
+        url: 'https://www.googleapis.com/youtube/v3/search',
+        data: {
+            key: 'AIzaSyAm8wLpmDSB6Bv6QoTuaAHBiw9wpjBIZyc',
+            q: songTitle,
+            part: 'snippet',
+            maxResults: 1,
+            order: 'relevance',
+            type: 'video',
+            videoEmbeddable: true
+        },
+        success: function(data){
+            console.log(data)
+            videoShow(data);
+        },
+        error: function(response){
+            console.log("Request Failed");
+        }
+      });
+    
+}
+
+var videoShow = function(data){
+
+    $("iframe").attr("src","http://www.youtube.com/embed/"+data.items[0].id.videoId);
 }
